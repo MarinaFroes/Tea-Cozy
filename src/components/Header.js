bdb121b87270dca586ac88fa6592159bafbdad95
MFAC <ninafroes@gmail.com>
2019-08-23 11:54:02 +0200

    feat: Create links to page sections

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,11 +20,62 @@ const Img = styled.img`
   margin-left: 1rem;
 `;
 
+const NavContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-end;
+  padding: 10px;
+  width: 100%;
+`;
+
+const NavLink = styled.a`
+  color: #fff;
+  text-decoration: none;
+  margin-right: 20px;
+  font-size: 1.2rem;
+  &:hover {
+    text-decoration: underline;
+  }
+  &:last-of-type {
+    margin-right: 40;
+  }
+`;
+
+const LINKS = [
+  {
+    label: "Mission",
+    href: "#mission"
+  },
+  {
+    label: "Gallery",
+    href: "#gallery"
+  },
+  {
+    label: "Locations",
+    href: "#locations"
+  },
+  {
+    label: "Contact",
+    href: "#contact"
+  }
+];
+
 export default function Header() {
   return (
     <HeaderContainer>
-      <Img src={logo} alt="logo"/>
-      <p>links</p>
+      <a href="#"><Img src={logo} alt="logo" /></a>
+      <NavContainer >
+        <nav>
+          {LINKS.map(link => (
+            <NavLink
+              key={link.href}
+              href={link.href}
+            >
+              {link.label}
+            </NavLink>
+          ))}
+        </nav>
+      </NavContainer>
     </HeaderContainer>
   )
 };

8bb96d822d57bf8faddcf9e365d0ce5103680ab2
MFAC <ninafroes@gmail.com>
2019-08-23 09:45:35 +0200

    feat: Add images and title

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,21 +1,50 @@
 import React from "react";
 import styled from "styled-components";
+import bedford from "../images/img-bedford-bizarre.jpg";
+import berryblitz from "../images/img-berryblitz.jpg";
+import myrtle from "../images/img-myrtle-ave.jpg";
+import donut from "../images/img-donut.jpg";
+import rum from "../images/img-spiced-rum.jpg";
 
 const GalleryContainer = styled.section`
   display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-  text-align: center;
+  flex-direction: column;
   background-color: #000;
-  color: #fff; 
+  border: 3px solid red;
+  text-align: center;
 `;
 
-export default function Locations() {
+const ImgContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  align-items: center;
+  text-align: center;
+  padding: 3rem;
+  border: 3px solid green;
+`;
+
+const Img = styled.img`
+  height: 13rem;
+  padding: 0.5rem;
+`;
+
+const SectionTitle = styled.h2`
+  font-size: 2rem;
+  color: #fff;
+`;
+
+export default function Gallery() {
   return (
     <GalleryContainer>
-      <img src="../images/img-bedford-bizarre.jpg" alt="tea"/>
-      <img src="../images/img-berryblitz.jpg" alt="tea"/>
-      <img src="../images/img-myrtle-ave.jpg" alt="tea"/>
+      <SectionTitle>Tea of the Month</SectionTitle>
+      <ImgContainer>
+        <Img src={bedford} alt="tea"/>
+        <Img src={berryblitz} alt="tea"/>
+        <Img src={myrtle} alt="tea" />
+        <Img src={donut} alt="tea" />
+        <Img src={rum} alt="tea" />
+      </ImgContainer>
     </GalleryContainer>
   )
 };

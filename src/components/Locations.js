b4b5ee2d7e613fa4978c273d7782a0046a3e0e9e
MFAC <ninafroes@gmail.com>
2019-08-23 10:32:47 +0200

    feat: Add background image and addresses

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -1,27 +1,68 @@
 import React from "react";
 import styled from "styled-components";
+import imgLocation from "../images/img-locations-background.jpg";
 
 const LocationsContainer = styled.section`
   display: flex;
-  justify-content: space-evenly;
+  flex-direction: column;
+  background: #000 url(${imgLocation}) center/cover no-repeat ;
+  text-align: center;
+  border: 3px solid yellow;
+`;
+
+const AddressContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
   align-items: center;
   text-align: center;
+  padding: 3rem;
+  border: 3px solid green;
+`;
+
+const Address = styled.div`
+  width: 10rem;
+  min-height: 12rem;
+  text-align: center;
   background-color: #000;
-  color: #fff; 
+  margin: 2rem;
+  color: #fff;
+  padding: 1rem;
+  border: 3px solid blue;
+`;
+
+const SectionTitle = styled.h2`
+  font-size: 2rem;
+  border: 3px solid blue;
+  margin: 0;
+  margin-top: 1rem;
+  color: #fff;
 `;
 
 export default function Locations() {
   return (
     <LocationsContainer>
-      <div>
-        Address 1
-      </div>
-      <div>
-        Address 2
-      </div>
-      <div>
-        Address 3
-      </div>
+      <SectionTitle>Locations</SectionTitle>
+      <AddressContainer>
+        <Address>
+          <h3>Downtown</h3>
+          <p>384 West 4th St</p>
+          <p>Suite 108</p>
+          <p>Portland, Maine</p>
+        </Address>
+        <Address>
+          <h3>East Bayside</h3>
+          <p>3433 Phisherman's Avenue</p>
+          <p>(Northwest Corner)</p>
+          <p>Portland, Maine</p>
+        </Address>
+        <Address>
+          <h3>Oakdale</h3>
+          <p>515 Crescent Avenue</p>
+          <p>Second Floor</p>
+          <p>Portland, Maine</p>
+        </Address>
+      </AddressContainer>
     </LocationsContainer>
   )
 };

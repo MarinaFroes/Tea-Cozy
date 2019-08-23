8aad60ae0cd6cef5836d13d4c2ca1ca007dabc0c
MFAC <ninafroes@gmail.com>
2019-08-23 11:26:25 +0200

    feat: Add background image and text

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,24 +1,48 @@
 import React from "react";
 import styled from "styled-components";
+import missionBg from "../images/img-mission-background.jpg";
 
 const HeroImage = styled.section`
   display: flex;
+  justify-content: center;
+  align-items: center;
   width: 100%;
-  background: #000 url(../images/img-mission-background.jpg) no-repeat fixed center; 
+  height: 100vh;
+  background-image: url(${missionBg}); 
+  background-size: cover;
 `;
 
-//  background-image: url(../images/img-mission-background.jpg);
 const MissionContainer = styled.div`
   display: flex;
+  flex-direction: column;
+  align-items: center;
   text-align: center;
-  background-color: #000;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.8);
   color: #fff;
+`;
+
+const SectionTitle = styled.h2`
+  font-size: 2rem;
+  border: 3px solid blue;
+  margin: 0;
+  margin-top: 1rem;
+`;
+
+const Paragraph = styled.p`
+  font-size: 1.5rem;
+  border: 3px solid blue;
+  margin: 0;
+  margin-bottom: 1rem;
 `;
 
 export default function Mission() {
   return (
     <HeroImage>
-      <MissionContainer>Mission</MissionContainer>
+      <MissionContainer>
+        <SectionTitle>Our Mission</SectionTitle>
+        <Paragraph>Handpicked, Artisanally Curated, Free Range, Sustainable, Small Batch, Fair Trade, Organic Tea</Paragraph>
+      </MissionContainer>
     </HeroImage>
   )
 };
